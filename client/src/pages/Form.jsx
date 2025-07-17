619b7d5386f9adffc22b93252eb0edfbfc048bf0
Him Kishan Das <70309670+Him-Kishan-Das@users.noreply.github.com>
2025-07-17 18:56:21 +0530

    form Type and steps read from the json and dynamically rendered in the ui

--- client/src/pages/Form.jsx
+++ client/src/pages/Form.jsx
@@ -1,30 +1,68 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import axios from "axios";
 import "./styles/Form.css";
 
 const Form = () => {
+  const { serviceId } = useParams(); // Extract serviceId from URL
+  const [formData, setFormData] = useState([]); // Use empty array
+
   useEffect(() => {
     const fetchFormData = async () => {
       try {
         const baseURL = process.env.REACT_APP_API_BASE_URL;
         console.log("Base URL:", baseURL);
-        const response = await axios.get(`${baseURL}/forms/service/10010`);
+        const response = await axios.get(`${baseURL}/forms/service/${serviceId}`);
+        setFormData(response.data[0]);
         console.log("API Response:", response.data);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
 
-    fetchFormData();
-  }, []);
-  
+    if (serviceId) {
+      fetchFormData();
+    }
+  }, [serviceId]);
+
+  if (FormData === null){
+    return <div className="container">Loading form...</div>;
+  }
+
+  const formName = formData.formName ? formData.formName.trim() : null;
+  const description = formData.description ? formData.description.trim() : null;
+  const formType = formData.type ? formData.type.trim() : null;
+  console.log(formType);
+
   return (
-    <>
-      <div className="container">
-        {/* formName */}
-        <h1>Issuance of Migration Certificate</h1>
-      </div>
-    </>
+    <div className="container">
+      <h1 className="formName">{formName}</h1>
+      <p className="description">{description}</p>
+
+
+      {formType === "stepper" && (
+        <div className="stepper-content">
+          <h2>Step-by-step Process</h2>
+
+          {formData.steps && formData.steps.length > 0 ? (
+            <ol className="stepper-list">
+
+              {formData.steps.map((step, index) => (
+                <li className="stepper-step" key={index}>
+                  <h3>{index + 1}</h3>
+                  <p>{step.header}</p>
+                </li>
+              ))}
+
+            </ol>
+          ) :
+          <p>No Steps defined for this stepper form</p>
+          }
+        </div>
+      )}
+
+      <button className="proceed">Proceed to Apply</button>
+    </div>
   );
 };
 
