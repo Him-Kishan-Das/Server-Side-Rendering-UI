6de02f251903aab5d5c93d6f1fa3b1f925a19356
Him Kishan Das <70309670+Him-Kishan-Das@users.noreply.github.com>
2025-08-10 12:36:16 +0530

    added the function when system back button is clicked

--- client/src/components/navigationButtons/SystemBack.js
+++ client/src/components/navigationButtons/SystemBack.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 const SystemBack = ({ button }) => {
+    const { serviceId } = useParams();
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate(`/forms/service/${serviceId}`);
+    };
+
     return (
-        <>
-            <button className="nav-button" type="button" id={button.type}>{button.enLabel}</button>
-        </>
-    )
-}
+        <button 
+            className="nav-button" 
+            type="button" 
+            id={button.type}
+            onClick={handleClick}
+        >
+            {button.enLabel}
+        </button>
+    );
+};
 
 export default SystemBack;
